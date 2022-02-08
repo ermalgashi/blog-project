fef9b69ec627e3e91bc4151f653c7a898ebae02e
Ermal Gashi <ermalmramori@gmail.com>
2022-02-08 10:13:21 +0100

    Fourth test for pre-commit

--- blogproject/urls.py
+++ blogproject/urls.py
@@ -1,6 +1,6 @@
 """blogproject URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see: Also something else
+The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
