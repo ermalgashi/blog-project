b3a5315ae299ca721d1c06b5d589a3222dfc6533
Ermal Gashi <ermalmramori@gmail.com>
2022-02-16 19:44:44 +0100

    Configured templates, made changes to view return detailed view based on pk

--- posts/urls.py
+++ posts/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.all_posts, name="all_posts"),
+    path("<int:id>/", views.get_post, name="get_post"),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,20 @@
+import imp
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Posts
 
 # Create your views here.
 
 
 def all_posts(request):
     return HttpResponse("<html><body>It is now.</body></html>")
+
+
+def get_post(request, id):
+    posts = Posts.objects.get(pk=id)
+    context = {
+        "title": posts.title,
+        "content": posts.content,
+        "author": posts.author,
+    }
+    return render(request, "posts/detail_view.html", context)
