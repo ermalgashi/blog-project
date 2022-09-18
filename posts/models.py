0928ac9c396917d4ea2454f77bbda02798163334
Ermal Gashi <ermalmramori@gmail.com>
2022-09-18 14:23:17 +0200

    Started the templates for list view and detail view, changed models added new columns to keep track of dates for edited posts and when created.

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,4 @@
+from pyexpat import model
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -7,6 +8,9 @@ class Posts(models.Model):
     title = models.CharField(max_length=256)
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    last_edited = models.DateTimeField(auto_now=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    draft = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
--- posts/views.py
+++ posts/views.py
@@ -1,13 +1,13 @@
-import imp
 from django.shortcuts import render
-from django.http import HttpResponse
+
 from .models import Posts
 
 # Create your views here.
 
 
 def all_posts(request):
-    return HttpResponse("<html><body>It is now.</body></html>")
+    posts = Posts.objects.all()
+    return render(request, "posts/list_view.html", context={"posts": posts})
 
 
 def get_post(request, id):
@@ -16,5 +16,6 @@ def get_post(request, id):
         "title": posts.title,
         "content": posts.content,
         "author": posts.author,
+        "last_edited": posts.last_edited,
     }
     return render(request, "posts/detail_view.html", context)
