f3df395bd39ec05efb743294a06da90dce5224fb
Ermal Gashi <ermalmramori@gmail.com>
2022-09-21 19:10:09 +0200

    Made changes to add logic so that we can view posts based on a category, edited the list view also the list template, made changes to the urls to allow for parameters in the list view

--- posts/urls.py
+++ posts/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.all_posts, name="all_posts"),
-    path("<int:id>/", views.get_post, name="get_post"),
+    path("<int:cat_id>/", views.all_posts, name="get_posts_by_category"),
+    path("<int:cat_id>/<int:id>/", views.get_post, name="get_post"),
     path("<int:id>/edit", views.edit_post, name="edit_post"),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,17 +1,23 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import PostForm
 
-from .models import Posts
+from .models import Posts, Category
 
 # Create your views here.
 
 
-def all_posts(request):
-    posts = Posts.objects.all()
+def all_posts(request, cat_id=None):
+    posts = None
+    if cat_id is not None:
+        c_page = get_object_or_404(Category, id=cat_id)
+        posts = Posts.objects.filter(category=c_page)
+    else:
+        posts = Posts.objects.all()
+
     return render(request, "posts/list_view.html", context={"posts": posts})
 
 
-def get_post(request, id):
+def get_post(request, cat_id, id):
     post = Posts.objects.get(pk=id)
     context = {
         "id": post.id,
@@ -30,7 +36,7 @@ def edit_post(request, id):
         form = PostForm(request.POST, instance=post)
         if form.is_valid():
             form.save()
-            return redirect("get_post", post.id)
+            return redirect("get_post", post.category.id, post.id)
     else:
         form = PostForm(instance=post)
 
